7c07f4644861dddd2309437cc81c2b9542d86aa2
ebreitmo <elena@epcc.ed.ac.uk>
2018-03-12 15:47:27 +0000

    first commit

---
+++ source/repos/Calculator/Calculator/ICalculator.cs
@@ -0,0 +1,15 @@
+﻿namespace Calculator
+{
+    public interface ICalculator
+    {
+        int Add(int n1, int n2);
+    }
+    public class Calculator : ICalculator
+    {
+        public int Add(int n1, int n2)
+        {
+        int result = n1 - n2;
+        return result;
+        }
+     }
+}
---
+++ source/repos/Calculator/Calculator/Program.cs
@@ -0,0 +1,19 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ICalculator sut = new Calculator();
+            int resultAdd = sut.Add(4, 5);
+
+            Console.WriteLine("Result: " + resultAdd.ToString());
+        }
+    }
+}
