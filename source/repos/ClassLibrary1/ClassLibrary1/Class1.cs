0d0c2ca1678a192685d4b7fee8ec7421584571e8
ebreitmo <elena@epcc.ed.ac.uk>
2018-03-12 15:44:36 +0000

    first commit

---
+++ source/repos/ClassLibrary1/ClassLibrary1/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Calculator;
+
+namespace ClassLibrary1
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void ShouldAddTwoNumbers()
+        {
+            ICalculator sut = new Calculator.Calculator();
+            int expectedRes = sut.Add(6,7);
+            Assert.That(expectedRes, Is.EqualTo(13));
+        }
+
+    }
+}
